b8bef8e94fe4ae2da8e4f9c03255e4897a58cba2
RikuAlter <rikuAlter@gmail.com>
2023-02-21 14:02:29 +0530

    [TSCS-04] Added PATCH mapping to allow update of traffic light pole sensor values

--- src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/TrafficSignalServiceController.java
+++ src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/TrafficSignalServiceController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +19,18 @@ public class TrafficSignalServiceController {
 	@Autowired
 	private TrafficSignalDAOService trafficSignalDAOService;
 	
-	@GetMapping(path = "/sensors")
-	@RequestMapping("/view-all")
+	@RequestMapping("/sensors")
 	public List<PoleData> retrieveAllTrafficPoleData(){
 		return trafficSignalDAOService.findAll();
 	}
 	
-	@GetMapping(path = "/sensor/{id}")
+	@GetMapping(path = "/sensors/{id}")
 	public PoleData retrievePoleDataById(@PathVariable String id) {
 		return trafficSignalDAOService.findById(id);
+	}
+	
+	@PatchMapping(path = "/sensors")
+	public void updatePoleData(@RequestBody PoleData sensor) {
+		trafficSignalDAOService.update(sensor);
 	}
 }
--- src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/dao/TrafficSignalDAOService.java
+++ src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/dao/TrafficSignalDAOService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 import com.springboot.traffic.sensor.control.service.trafficcontrolservice.dtos.PoleData;
 
 @Component
+@Transactional
 public class TrafficSignalDAOService{
 	
 	@Autowired
@@ -19,13 +21,11 @@ public class TrafficSignalDAOService{
 		return entityManager.createNamedQuery("PoleData.findAll", PoleData.class).getResultList();
 	}
 	
-	public PoleData findById(String id) {
+	public PoleData findById(final String id) {
 		return entityManager.find(PoleData.class, id);
 	}
 	
-	public PoleData update(String id, String status) {
-		final PoleData sensor = findById(id);
-		sensor.setStatus(status);
+	public PoleData update(final PoleData sensor) {
 		return entityManager.merge(sensor);
 	}
 }
