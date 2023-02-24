6944a3ce39acad480cfae3891dace20d6a8cbddf
RikuAlter <rikuAlter@gmail.com>
2023-02-25 03:52:14 +0530

    [TSCS-08] Producer service to upload data from sensors to kapka topic

---
+++ traffic-sensor-producer/src/main/java/com/traffic/sensor/data/producer/trafficsensorproducer/TrafficSensorProducerApplication.java
@@ -0,0 +1,13 @@
+package com.traffic.sensor.data.producer.trafficsensorproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSensorProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficSensorProducerApplication.class, args);
+	}
+
+}
---
+++ traffic-sensor-producer/src/test/java/com/traffic/sensor/data/producer/trafficsensorproducer/TrafficSensorProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traffic.sensor.data.producer.trafficsensorproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficSensorProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
