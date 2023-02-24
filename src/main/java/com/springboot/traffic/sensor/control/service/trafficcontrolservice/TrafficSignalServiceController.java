accb35e189a23a059d89bf18dcf9bfad3190c0c2
RikuAlter <rikuAlter@gmail.com>
2023-02-24 23:37:46 +0530

    [TSCS-06] Decoupled H2 DB from the microservice and replaced H2 with Postgresql15

--- src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/TrafficSignalServiceController.java
+++ src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/TrafficSignalServiceController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.traffic.sensor.control.service.trafficcontrolservice.dao.TrafficSignalDAOService;
@@ -19,7 +18,7 @@ public class TrafficSignalServiceController {
 	@Autowired
 	private TrafficSignalDAOService trafficSignalDAOService;
 	
-	@RequestMapping("/sensors")
+	@GetMapping("/sensors")
 	public List<Sensor> retrieveAllTrafficPoleData(){
 		return trafficSignalDAOService.findAll();
 	}
