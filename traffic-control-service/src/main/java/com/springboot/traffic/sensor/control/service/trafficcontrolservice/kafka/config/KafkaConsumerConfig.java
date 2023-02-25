d70cab92ff5b806c83b5f06ed5057a646e7e5dda
Rahul Sarkar <110712643+RikuAlter@users.noreply.github.com>
2023-02-25 13:14:27 +0530

    Enable kafka configuration

--- traffic-control-service/src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/kafka/config/KafkaConsumerConfig.java
+++ traffic-control-service/src/main/java/com/springboot/traffic/sensor/control/service/trafficcontrolservice/kafka/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -14,6 +15,7 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import com.springboot.traffic.sensor.control.service.trafficcontrolservice.dtos.Sensor;
 
 @Configuration
+@EnableKafka
 public class KafkaConsumerConfig {
 
 	@Bean
